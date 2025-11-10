f42c82967e7ec74e308f1fd91e2d3f9e522fe093
Noah <github@mepost.org>
2025-11-10 18:31:32 +0100

    harpoon

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    keys = {
+            { "<leader>a", function() require("harpoon"):list():add() end},
+            { "<leader>h", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end},
+            { "<C-1>", function() require("harpoon"):list():select(1) end},
+            { "<C-2>", function() require("harpoon"):list():select(2) end},
+            { "<C-3>", function() require("harpoon"):list():select(3) end},
+            { "<C-4>", function() require("harpoon"):list():select(4) end},
+    }
+}
+
