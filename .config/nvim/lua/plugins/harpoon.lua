88520d7ca02beaa8def4354014cb8ffc9e4ce7b8
Noah <github@mepost.org>
2025-11-16 19:54:48 +0100

    changed keybindings

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -6,10 +6,10 @@ return {
     keys = {
             { "<leader>a", function() require("harpoon"):list():add() end},
             { "<leader>h", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end},
-            { "<C-1>", function() require("harpoon"):list():select(1) end},
-            { "<C-2>", function() require("harpoon"):list():select(2) end},
-            { "<C-3>", function() require("harpoon"):list():select(3) end},
-            { "<C-4>", function() require("harpoon"):list():select(4) end},
+            { "<leader>1", function() require("harpoon"):list():select(1) end},
+            { "<leader>2", function() require("harpoon"):list():select(2) end},
+            { "<leader>3", function() require("harpoon"):list():select(3) end},
+            { "<leader>4", function() require("harpoon"):list():select(4) end},
     }
 }
 
