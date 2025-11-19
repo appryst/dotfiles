8a5a961d8ad34ba98d2f565a4db83c87ce09afae
Noah <github@mepost.org>
2025-11-19 19:11:32 +0100

    send to quickfix

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -4,11 +4,20 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     -- or if using mini.icons/mini.nvim
     -- dependencies = { "nvim-mini/mini.icons" },
-    opts = {},
+    opts = {
+        keymap = {
+            fzf = {
+                true,
+                -- Use <c-q> to select all items and add them to the quickfix list
+                ["ctrl-q"] = "select-all+accept",
+            }
+        }
+    },
     keys = {
         { "<leader>ff", function() require("fzf-lua").files() end, desc = "Find files" },
         { "<leader>fp", function() require("fzf-lua").git_files() end, desc = "Find files" },
         { "<leader>fg", function() require("fzf-lua").live_grep() end, desc = "Live grep" },
         { "<leader>fb", function() require("fzf-lua").buffers() end, desc = "Buffers" },
+        { "<leader>xx", function() require("fzf-lua").diagnostics_workspace() end, desc = "Diagonstics Workspace" },
     },
 }
