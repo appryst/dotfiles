8b21ff58c399ff24eaa65df72628ab1f41ea5e48
Noah <github@mepost.org>
2025-11-19 19:11:25 +0100

    floats transparent

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -7,6 +7,9 @@ return {
     config = function()
         require("catppuccin").setup({
             transparent_background = true,
+            float = {
+                transparent = true,
+            }
         })
         vim.cmd.colorscheme "catppuccin-mocha"
     end
