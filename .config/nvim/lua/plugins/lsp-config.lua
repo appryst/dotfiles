7dccee95c5d9c7ec97ad1ceecb36eea5d713646d
Noah <github@mepost.org>
2025-11-25 17:39:16 +0100

    changed symbols signs diagnostics

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -25,6 +25,15 @@ return {
 
 			vim.diagnostic.config({
 				virtual_text = true,
+                severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.INFO] = "",
+						[vim.diagnostic.severity.HINT] = "󰌶",
+					},
+				},
 			})
 		end,
 	},
