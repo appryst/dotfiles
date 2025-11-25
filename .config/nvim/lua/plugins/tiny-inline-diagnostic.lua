f17a84af075dc1005576efd6319d320b4726e917
Noah <github@mepost.org>
2025-11-25 17:39:37 +0100

    improved look

--- .config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ .config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -6,6 +6,7 @@ return {
 		require("tiny-inline-diagnostic").setup({
 			preset = "classic",
 			transparent_bg = true,
+            transparent_cursorline = false,
 			options = {
 				multilines = {
 					enabled = true,
