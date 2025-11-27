f9026797864c255028ba443c4ec24b4a21391f0f
Noah <github@mepost.org>
2025-11-27 17:56:58 +0100

    snippets

---
+++ .config/nvim/lua/snippets/typst.lua
@@ -0,0 +1,21 @@
+---@diagnostic disable: undefined-global
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+return {
+	-- math modes
+	s({ trig = "mt", snippetType = "autosnippet" },
+		fmta("$<>$ ", { i(1) })
+	),
+	s({ trig = "mmt", snippetType = "autosnippet" },
+		fmta("$ <> $ ", { i(1) })
+	),
+	s({ trig = "cent" },
+		fmta("#align(center)[<>]", { i(1) })
+	),
+	s({ trig = "v" },
+		fmta("#let <> = <>", { i(1), i(2) })
+	),
+}
