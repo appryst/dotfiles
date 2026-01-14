0fa5ebb097a5087f6fadda7022c242262f5464f4
Noah <github@mepost.org>
2026-01-14 16:26:33 +0100

    new syntax git.yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,7 +2,7 @@ th.git = th.git or {}
 
 th.git.modified = ui.Style():fg("#E5C07B")
 th.git.added    = ui.Style():fg("#98C379")
-th.git.untracked = ui.Style():fg("#FF6B6B")       -- Helleres Rot für untracked
+th.git.untracked = ui.Style():fg("#FF6B6B")
 th.git.ignored  = ui.Style():fg("#5C6370")
 th.git.deleted  = ui.Style():fg("#BE5046"):bold()
 th.git.updated  = ui.Style():fg("#56B6C2")
