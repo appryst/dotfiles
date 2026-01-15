9494134d7b71b5247fe12d8c66a3b492fca0d181
Noah <github@mepost.org>
2026-01-15 15:55:37 +0100

    jsonc

--- .config/hypr/scripts/rofi-file-search.sh
+++ .config/hypr/scripts/rofi-file-search.sh
@@ -8,7 +8,7 @@ selection=$(fd . --hidden --type f "$HOME" 2>/dev/null | \
 if [ -n "$selection" ]; then
     filename="${selection##*/}"
     case "${selection##*.}" in
-        txt|md|markdown|c|cpp|cc|cxx|css|h|hpp|hh|hxx|py|java|js|ts|tsx|jsx|sh|bash|zsh|fish|lua|vim|tex|rb|pl|pm|php|go|rs|swift|kt|kts|scala|r|jl|sql|json|yaml|yml|ini|conf|cfg|dockerfile|make|mk|gradle|groovy|bat|rasi|ps1)
+        txt|md|markdown|c|cpp|cc|cxx|css|h|hpp|hh|hxx|py|java|js|ts|tsx|jsx|sh|bash|zsh|fish|lua|vim|tex|rb|pl|pm|php|go|rs|swift|kt|kts|scala|r|jl|sql|json|jsonc|yaml|yml|ini|conf|cfg|dockerfile|make|mk|gradle|groovy|bat|rasi|ps1)
             ghostty -e nvim "$selection"
             ;;
         *)
