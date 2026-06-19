b3ebc5db25dd0caf06409be278179c45c778d517
Noah <github@mepost.org>
2026-06-19 11:19:08 +0200

    format

--- .config/nvim/plugin/snacks.lua
+++ .config/nvim/plugin/snacks.lua
@@ -1,14 +1,22 @@
 vim.pack.add({
-  'https://github.com/folke/snacks.nvim',
+	"https://github.com/folke/snacks.nvim",
 })
 
 require("snacks").setup({
-  picker = { enabled = true },
-  statuscolumn = { enabled = true, left = { "sign" } }
+	picker = { enabled = true },
+	statuscolumn = { enabled = true, left = { "sign" } },
 })
 
 -- Snacks keymaps
-vim.keymap.set("n", "<leader><space>", function() Snacks.picker.smart() end, { desc = "Smart Find Files" })
-vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", function() Snacks.picker.grep() end, { desc = "Grep" })
-vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader><space>", function()
+	Snacks.picker.smart()
+end, { desc = "Smart Find Files" })
+vim.keymap.set("n", "<leader>ff", function()
+	Snacks.picker.files()
+end, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", function()
+	Snacks.picker.grep()
+end, { desc = "Grep" })
+vim.keymap.set("n", "<leader>fb", function()
+	Snacks.picker.buffers()
+end, { desc = "Buffers" })
