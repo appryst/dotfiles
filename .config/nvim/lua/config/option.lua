4776e8069461477a959e5f8fc6967c9e706a107a
Noah <github@mepost.org>
2026-07-07 12:24:40 +0200

    added workspace diagnostics

--- .config/nvim/lua/config/option.lua
+++ .config/nvim/lua/config/option.lua
@@ -54,3 +54,14 @@ vim.lsp.config('copilot', {
     }
   }
 })
+
+vim.lsp.config('*', {
+  on_attach = function(client, bufnr)
+                -- some clients support workspace diagnostics natively
+                if client:supports_method("workspace/diagnostic", bufnr) then
+                  vim.lsp.buf.workspace_diagnostics({ client_id = client.id })
+                else
+                  require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
+                end
+              end
+})
